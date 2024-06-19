a36430ff28dd95be4dc9e2f5bf95347cab6b8ad9
osmarrjr <osmar.rjr@hotmail.com>
2024-06-19 00:42:23 -0300

    adding sidebar component

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,20 @@ import { Post } from "./Post";
 import { Header } from "./components/Header";
 
 import "./global.css";
+import styles from "./App.module.css";
+import { Sidebar } from "./components/Sidebar";
 
 export function App() {
   return (
     <div>
       <Header />
-      <Post author="Osmar Roncasalia" content="Lorem Ipsum" />
-      <Post author="Osmar Roncasalia" content="Lorem Ipsum" />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post author="Osmar Roncasalia" content="Lorem Ipsum" />
+          <Post author="Osmar Roncasalia" content="Lorem Ipsum" />
+        </main>
+      </div>
     </div>
   );
 }
