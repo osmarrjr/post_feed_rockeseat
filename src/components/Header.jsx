4b93afb7882581370bf0a8b5893bddfa37a82b86
osmarrjr <osmar.rjr@hotmail.com>
2024-06-19 00:02:59 -0300

    adding logo image to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,11 @@
-import styles from "./Header.module.css"
+import styles from "./Header.module.css";
 
-export function Header () {
-    return (
-        <header className={styles.header}>
-            <strong >Ignite Feed</strong>
-        </header>
-    );
+import igniteLogo from "../assets/ignite-logo.svg";
+
+export function Header() {
+  return (
+    <header className={styles.header}>
+      <img src={igniteLogo} alt="Logotipo do Ignite" />
+    </header>
+  );
 }
